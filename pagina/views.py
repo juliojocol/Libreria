2769d27e3b5684bf118f6f83e181e5d892b5a884
Julio Jocol <juliojocol12@mesoamericana.edu.gt>
2021-11-02 19:53:23 -0600

    Creacion de la primera vista y template

--- pagina/views.py
+++ pagina/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Publicacion
 
-# Create your views here.
+def publicacion_lista(request):
+    publicaciones = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'pagina/publicacion_lista.html', {'publicaciones':publicaciones})
