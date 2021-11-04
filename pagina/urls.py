5f56c2ccf580aac53afcf42100d9869e8a896a61
Julio Jocol <juliojocol12@mesoamericana.edu.gt>
2021-11-03 18:16:34 -0600

    Formularios de actualizar, eliminar y login

--- pagina/urls.py
+++ pagina/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('articulo/<int:pk>/', views.publicacion_detalle, name='publicacion_detalle'),
     path('articulo/nueva/', views.publicacion_nueva, name='publicacion_nueva'),
     path('publicacion/<int:pk>/editar/', views.publicacion_editar, name='publicacion_editar'),
+    path('borradores/', views.publicacion_borrador_lista, name='publicacion_borrador_lista'),
+    path('publicacion/<int:pk>/publicar', views.publicacion_publicar, name='publicacion_publicar'),
+    path('publicacion/<int:pk>/eliminar', views.publicacion_eliminar, name='publicacion_eliminar'),
     
 ]
